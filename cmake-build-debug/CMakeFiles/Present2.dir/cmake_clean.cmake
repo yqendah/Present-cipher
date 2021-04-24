9c4ef014a6f0a7f786b57e6e1c7e254c2858a2f6
yqendah <yaman.a.fares@gmail.com>
2021-04-24 15:20:39 +0300

    init commit

---
+++ cmake-build-debug/CMakeFiles/Present2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Present2.dir/main.c.obj"
+  "Present2.exe"
+  "Present2.exe.manifest"
+  "Present2.pdb"
+  "libPresent2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Present2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
